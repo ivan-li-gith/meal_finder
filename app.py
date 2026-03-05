a18be0868c506a215da31db652aa4aba4343904e
ivan li <ivanli9355@gmail.com>
2026-03-05 10:40:34 -0800

    added a favorites page

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from preferences import get_preferences
 import streamlit as st
-from pantry import get_pantry_recipes
+from pantry import get_current_ig, find_recipes
 from substitution import substitution_page
+from favorites import favorites_page
 
 if 'page' not in st.session_state:
     st.session_state['page'] = 'Preferences'
     
 st.sidebar.title("Meal Optimizer")
-selection = st.sidebar.radio("Go to", ["Preferences", "My Pantry", "Substitutions"])
+selection = st.sidebar.radio("Go to", ["Preferences", "Find a Recipe", "Substitutions", "Favorites"])
 
 st.session_state['page'] = selection
     
 if st.session_state['page'] == 'Preferences':
     get_preferences()
-elif st.session_state['page'] == 'My Pantry':
-    get_pantry_recipes()
+elif st.session_state['page'] == 'Find a Recipe':
+    get_current_ig()
+    find_recipes()
 elif st.session_state['page'] == 'Substitutions':
     substitution_page()
+elif st.session_state['page'] == 'Favorites':
+    favorites_page()
